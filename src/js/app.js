642d4fc8944d3eef733603cbe90fbe7e2dacd2cf
Nikola Orlovski <nikikiko1@abv.bg>
2022-12-17 12:42:29 +0200

    Added Fire Emoji for the Hot price

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  let hotPrices = document.querySelectorAll("p.hot")
+
+  for (let index = 0; index < hotPrices.length; index++) {
+    const element = hotPrices[index];
+    hotPrices[index].insertAdjacentText('beforeEnd'," 🔥")
+  }
 });
