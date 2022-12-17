ec959f8361e33f54436bdd55f7806030d67d800a
Nikola Orlovski <nikikiko1@abv.bg>
2022-12-17 12:47:35 +0200

    Added logic

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
   let hotPrices = document.querySelectorAll("p.hot")
 
   for (let index = 0; index < hotPrices.length; index++) {
